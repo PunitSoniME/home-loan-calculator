3e9fd4ff4b71587cadd2ff6cc83861d915e8b97f
Punit Soni <punit.soni33@gmail.com>
2022-10-09 17:30:32 +0530

    - New features added

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,32 @@ const formatNumber = (amount) => {
 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+const checkValueIsValid = (character) => {
+  var p = new RegExp(/^[0-9]+([.][0-9]+)?$/);
+  return Number(character) === 0 || p.test(Number(character));
+}
+
 function App() {
 
-  const [principal, setPrincipal] = useState(0);
-  const [interest, setInterest] = useState(0);
-  const [tenureInMonths, setTenureInMonths] = useState(0);
-  const [additionalAmount, setAdditionalAmount] = useState(0);
-  const [additionalAmountPerMonth, setAdditionalAmountPerMonth] = useState(0);
+  const [principal, setPrincipal] = useState("");
+  const [interest, setInterest] = useState("");
+  const [tenureInMonths, setTenureInMonths] = useState("");
+  const [additionalAmount, setAdditionalAmount] = useState("");
+  const [additionalAmountPerMonth, setAdditionalAmountPerMonth] = useState("");
 
+  const [totalInterest, setTotalInterest] = useState("0");
   const [emis, setEmis] = useState([]);
 
   const calculateInterest = () => {
-    let updatedPrincipal = principal;
-    let modifiedTenureInMonths = tenureInMonths;
+
+    let updatedPrincipal = Number(principal);
+    let modifiedTenureInMonths = Number(tenureInMonths);
     let counter = 1;
 
-    let updatedInterest = interest / 100 / 12;
+    let updatedInterest = Number(interest) / 100 / 12;
 
     var x = Math.pow(1 + updatedInterest, modifiedTenureInMonths);
-    let monthlyFixedEMI = Math.round((principal * x * updatedInterest) / (x - 1));
+    let monthlyFixedEMI = Math.round((Number(principal) * x * updatedInterest) / (x - 1));
 
     let data = [];
     let date = new Date();
@@ -37,15 +44,15 @@ function App() {
 
       data.push({
         monthYear: `${months[date.getMonth()]}-${date.getFullYear()}`,
-        additionalAmount: additionalAmount,
+        additionalAmount: Number(additionalAmount),
         principal: principalPerMonth,
         interest: interestPerMonth,
-        emi: (counter % additionalAmountPerMonth === 0) ? (principalPerMonth + additionalAmount + monthlyFixedEMI) : monthlyFixedEMI,
-        balance: (counter % additionalAmountPerMonth === 0) ? (updatedPrincipal - additionalAmount - principalPerMonth) : (updatedPrincipal - principalPerMonth)
+        emi: (counter % Number(additionalAmountPerMonth) === 0) ? (principalPerMonth + Number(additionalAmount) + monthlyFixedEMI) : monthlyFixedEMI,
+        balance: (counter % Number(additionalAmountPerMonth) === 0) ? (updatedPrincipal - Number(additionalAmount) - principalPerMonth) : (updatedPrincipal - principalPerMonth)
       });
 
-      if (counter % additionalAmountPerMonth === 0) {
-        principalPerMonth = principalPerMonth + additionalAmount;
+      if (counter % Number(additionalAmountPerMonth) === 0) {
+        principalPerMonth = principalPerMonth + Number(additionalAmount);
       }
 
       date.setMonth(date.getMonth() + 1);
@@ -56,14 +63,19 @@ function App() {
 
     } while (updatedPrincipal > 0);
 
-    if ((counter - 1) % additionalAmountPerMonth === 0 && Number(data[data.length - 1].balance) < 0) {
+    if ((counter - 1) % Number(additionalAmountPerMonth) === 0 && Number(data[data.length - 1].balance) < 0) {
       data[data.length - 1].additionalAmount = 0;
       data[data.length - 1].emi = monthlyFixedEMI;
       data[data.length - 1].balance = 0;
     }
 
-    setEmis([...data]);
+    let calculatedTotalInterest = 0
+    data.forEach(f => {
+      calculatedTotalInterest = calculatedTotalInterest + f.interest;
+    })
 
+    setTotalInterest(formatNumber(calculatedTotalInterest));
+    setEmis([...data]);
   }
 
   return (
@@ -72,24 +84,30 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="principal" className="form-label">Principal Amount *</label>
-            <input type="number" value={principal} onChange={(e) => {
-              setPrincipal(Number(e.currentTarget.value));
+            <label htmlFor="principal" className="form-label fw-semibold">Principal Amount *</label>
+            <input type="text" value={principal} onChange={(e) => {
+              if (checkValueIsValid(e.currentTarget.value)) {
+                setPrincipal(e.currentTarget.value);
+              }
             }} className="form-control" id="principal" placeholder="Principal Amount" />
           </div>
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="interest" className="form-label">Interest Rate % *</label>
-            <input type="number" value={interest} onChange={(e) => {
-              setInterest(Number(e.currentTarget.value));
+            <label htmlFor="interest" className="form-label fw-semibold">Interest Rate % *</label>
+            <input type="text" value={interest} onChange={(e) => {
+              if (checkValueIsValid(e.currentTarget.value)) {
+                setInterest(e.currentTarget.value);
+              }
             }} className="form-control" id="interest" placeholder="Interest Rate %" />
           </div>
         </div>
 
         <div className="row mb-3">
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="tenureInMonths" className="form-label">Tenure ( in months ) *</label>
-            <input type="number" value={tenureInMonths} onChange={(e) => {
-              setTenureInMonths(Number(e.currentTarget.value));
+            <label htmlFor="tenureInMonths" className="form-label fw-semibold">Tenure ( in months ) *</label>
+            <input type="text" value={tenureInMonths} onChange={(e) => {
+              if (checkValueIsValid(e.currentTarget.value)) {
+                setTenureInMonths(e.currentTarget.value);
+              }
             }} className="form-control" id="tenureInMonths" placeholder="Tenure ( in months )" />
           </div>
         </div>
@@ -99,16 +117,20 @@ function App() {
         <div className="row mb-3">
 
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="additionalAmount" className="form-label">Amount you want to add in monthly principal</label>
-            <input type="number" value={additionalAmount} onChange={(e) => {
-              setAdditionalAmount(Number(e.currentTarget.value));
+            <label htmlFor="additionalAmount" className="form-label fw-semibold">Amount you want to add in monthly principal</label>
+            <input type="text" value={additionalAmount} onChange={(e) => {
+              if (checkValueIsValid(e.currentTarget.value)) {
+                setAdditionalAmount(e.currentTarget.value);
+              }
             }} className="form-control" id="additionalAmount" placeholder="Amount you want to add in monthly principal" />
           </div>
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="additionalAmountPerMonth" className="form-label">After how many month you want to add additional amount ? ( recursive )</label>
-            <input type="number" value={additionalAmountPerMonth} onChange={(e) => {
-              setAdditionalAmountPerMonth(Number(e.currentTarget.value));
-            }} className="form-control" id="additionalAmountPerMonth" placeholder="After how many month you want to add additional amount ? ( recursive )" />
+            <label htmlFor="additionalAmountPerMonth" className="form-label fw-semibold">After how many months you want to add additional amount ? ( recursive )</label>
+            <input type="text" value={additionalAmountPerMonth} onChange={(e) => {
+              if (checkValueIsValid(e.currentTarget.value)) {
+                setAdditionalAmountPerMonth(e.currentTarget.value);
+              }
+            }} className="form-control" id="additionalAmountPerMonth" placeholder="After how many months you want to add additional amount ? ( recursive )" />
           </div>
 
         </div>
@@ -118,6 +140,12 @@ function App() {
         <button disabled={!(principal && interest && tenureInMonths)} type="button" className="btn btn-primary" onClick={calculateInterest}>Calculate</button>
 
         <hr />
+
+        {
+          emis.length > 0 ? <div class="alert alert-danger fw-bold" role="alert">
+            Total Interest - {totalInterest}
+          </div> : ""
+        }
 
         <div className="table-responsive">
           <table className="table table-bordered table-hover table-striped">
