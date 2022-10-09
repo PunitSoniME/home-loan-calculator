45b3dad8a3d615981fc5336e3b67fb9e0afa1473
Punit Soni <punit.soni33@gmail.com>
2022-10-09 14:52:17 +0530

    - Calculation updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 
+const formatNumber = (amount) => {
+  return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: amount.toString().length }).format(amount);
+}
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 function App() {
 
-  const [principal, setPrincipal] = useState(0);
-  const [interest, setInterest] = useState(0);
-  const [tenure, setTenure] = useState(0);
+  const [principal, setPrincipal] = useState(3933320);
+  const [interest, setInterest] = useState(8.15);
+  const [tenureInMonths, setTenureInMonths] = useState(240);
   const [additionalAmount, setAdditionalAmount] = useState(0);
   const [additionalAmountPerMonth, setAdditionalAmountPerMonth] = useState(0);
 
   const [emis, setEmis] = useState([]);
 
-  const formatNumber = (amount) => {
-    return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: amount.toString().length }).format(amount);
-  }
-
   const calculateInterest = () => {
     let updatedPrincipal = principal;
-    let modifiedTenure = tenure;
+    let modifiedTenureInMonths = tenureInMonths;
     let counter = 1;
 
     let updatedInterest = interest / 100 / 12;
 
-    var x = Math.pow(1 + updatedInterest, modifiedTenure);
+    var x = Math.pow(1 + updatedInterest, modifiedTenureInMonths);
     let monthlyFixedEMI = Math.round((principal * x * updatedInterest) / (x - 1));
 
     let data = [];
@@ -33,29 +35,36 @@ function App() {
       let interestPerMonth = Math.round(updatedInterest * updatedPrincipal);
       let principalPerMonth = Math.round(monthlyFixedEMI - interestPerMonth);
 
-      if (counter % additionalAmountPerMonth === 0) {
-        // if () {
-
-        // } else {
-        principalPerMonth = principalPerMonth + additionalAmount;
-        // }
-      }
-
       data.push({
-        monthYear: `${date.getMonth() + 1}-${date.getFullYear()}`,
-        principal: formatNumber(principalPerMonth),
-        interest: formatNumber(interestPerMonth),
-        emi: formatNumber((counter % additionalAmountPerMonth === 0) ? (principalPerMonth + monthlyFixedEMI) : monthlyFixedEMI),
-        balance: formatNumber(updatedPrincipal - principalPerMonth)
+        monthYear: `${months[date.getMonth()]}-${date.getFullYear()}`,
+        additionalAmount: additionalAmount,
+        principal: principalPerMonth,
+        interest: interestPerMonth,
+        emi: (counter % additionalAmountPerMonth === 0) ? (principalPerMonth + additionalAmount + monthlyFixedEMI) : monthlyFixedEMI,
+        balance: (counter % additionalAmountPerMonth === 0) ? (updatedPrincipal - additionalAmount - principalPerMonth) : (updatedPrincipal - principalPerMonth)
       });
+
+      if (counter % additionalAmountPerMonth === 0) {
+        principalPerMonth = principalPerMonth + additionalAmount;
+      }
 
       date.setMonth(date.getMonth() + 1);
       updatedPrincipal = updatedPrincipal - principalPerMonth;
 
-      modifiedTenure = modifiedTenure - 1;
+      modifiedTenureInMonths = modifiedTenureInMonths - 1;
       ++counter;
 
     } while (updatedPrincipal > 0);
+
+    if ((counter - 1) % additionalAmountPerMonth === 0 && Number(data[data.length - 1].balance) < 0) {
+      // principalPerMonth = principalPerMonth + additionalAmount;
+
+      data[data.length - 1].additionalAmount = 0;
+      data[data.length - 1].emi = monthlyFixedEMI;
+      data[data.length - 1].balance = 0;
+
+    }
+
 
     setEmis([...data]);
 
@@ -82,10 +91,10 @@ function App() {
 
         <div className="row mb-3">
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="tenure" className="form-label">Tenure ( in months ) *</label>
-            <input type="number" value={tenure} onChange={(e) => {
-              setTenure(Number(e.currentTarget.value));
-            }} className="form-control" id="tenure" placeholder="Tenure ( in months )" />
+            <label htmlFor="tenureInMonths" className="form-label">Tenure ( in months ) *</label>
+            <input type="number" value={tenureInMonths} onChange={(e) => {
+              setTenureInMonths(Number(e.currentTarget.value));
+            }} className="form-control" id="tenureInMonths" placeholder="Tenure ( in months )" />
           </div>
         </div>
 
@@ -100,41 +109,45 @@ function App() {
             }} className="form-control" id="additionalAmount" placeholder="Amount you want to add in monthly principal" />
           </div>
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="additionalAmountPerMonth" className="form-label">After how many month you want to add above amount ( recursive )</label>
+            <label htmlFor="additionalAmountPerMonth" className="form-label">After how many month you want to add additional amount ? ( recursive )</label>
             <input type="number" value={additionalAmountPerMonth} onChange={(e) => {
               setAdditionalAmountPerMonth(Number(e.currentTarget.value));
-            }} className="form-control" id="additionalAmountPerMonth" placeholder="After how many month you want to add above amount ( recursive )" />
+            }} className="form-control" id="additionalAmountPerMonth" placeholder="After how many month you want to add additional amount ? ( recursive )" />
           </div>
 
         </div>
 
         <hr />
 
-        <button type="button" className="btn btn-primary" onClick={calculateInterest}>Calculate</button>
+        <button disabled={!(principal && interest && tenureInMonths)} type="button" className="btn btn-primary" onClick={calculateInterest}>Calculate</button>
 
         <hr />
 
-        <div class="table-responsive">
+        <div className="table-responsive">
           <table className="table table-bordered table-hover table-striped">
             <thead>
               <tr>
-                <th width="300px">Month-Year</th>
-                <th width="300px">Principal</th>
-                <th width="300px">Interest</th>
-                <th width="300px">EMI</th>
-                <th width="300px">Balance</th>
+                <th>Month-Year</th>
+                <th>Principal</th>
+                <th>Interest</th>
+                <th>EMI</th>
+                <th>Balance</th>
               </tr>
             </thead>
 
             <tbody>
               {
                 emis.map((d, index) => (
-                  <tr key={index}>
-                    <td width="300px">{d.monthYear}</td>
-                    <td width="300px">{d.principal}</td>
-                    <td width="300px">{d.interest}</td>
-                    <td width="300px">{d.emi}</td>
-                    <td width="300px">{d.balance}</td>
+                  <tr key={index} className={d.monthYear.indexOf("Jan") > -1 ? 'fw-bold' : ''}>
+                    <td>{d.monthYear}</td>
+                    <td>{formatNumber(d.principal)}
+                      {
+                        d.additionalAmount && ((index + 1) % additionalAmountPerMonth === 0) ? ` + ${formatNumber(additionalAmount)}` : ""
+                      }
+                    </td>
+                    <td>{formatNumber(d.interest)}</td>
+                    <td>{formatNumber(d.emi)}</td>
+                    <td>{formatNumber(d.balance) < 0 ? "0" : formatNumber(d.balance)}</td>
                   </tr>
                 ))
               }
@@ -143,8 +156,6 @@ function App() {
         </div>
 
       </div>
-
-
 
     </div>
   )
