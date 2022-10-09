957cfe65d43d2104f0c926d850388812774a3a72
Punit Soni <punit.soni33@gmail.com>
2022-10-09 13:34:40 +0530

    - Configuration updated

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  build: {
-    outDir: './docs'
-  }
+  // root: './',
+  // build: {
+  //   outDir: './docs'
+  // }
 })
